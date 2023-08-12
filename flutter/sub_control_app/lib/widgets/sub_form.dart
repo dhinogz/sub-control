67206be625b6906405f0eb22452abac86bc089d4
rickyjasso <rickyjassoa@gmail.com>
2023-08-12 14:30:48 -0600

    Added dropdown picker for renewal cycle

--- flutter/sub_control_app/lib/widgets/sub_form.dart
+++ flutter/sub_control_app/lib/widgets/sub_form.dart
@@ -6,7 +6,6 @@ import 'package:sub_control_app/theme.dart';
 class AddSubscriptionForm extends StatefulWidget {
   // callback function to update the state with the new subscription
   final Function(SubscriptionData) onCreateSubscription;
-
   const AddSubscriptionForm({super.key, required this.onCreateSubscription});
 
   @override
@@ -18,8 +17,13 @@ class _AddSubscriptionFormState extends State<AddSubscriptionForm> {
   final providerController = TextEditingController();
   final priceController = TextEditingController();
   final paymentController = TextEditingController();
-  final cycleController = TextEditingController();
-
+  static const cycleValues = <String>[
+    '1 Month',
+    '3 Months',
+    '6 Months',
+    '1 Year'
+  ];
+  String dropdownValue = cycleValues.first;
   DateTime date = DateTime.now();
 
   // creates a new subscription object and clears the text fields
@@ -29,7 +33,7 @@ class _AddSubscriptionFormState extends State<AddSubscriptionForm> {
         price: double.parse(priceController.text),
         renewalDate: date.showFormattedDate(),
         paymentMethod: paymentController.text,
-        paymentCycle: cycleController.text);
+        paymentCycle: dropdownValue);
 
     // calls the callback function to update the state with the new subscription
     widget.onCreateSubscription(newSub);
@@ -38,7 +42,6 @@ class _AddSubscriptionFormState extends State<AddSubscriptionForm> {
     providerController.clear();
     priceController.clear();
     paymentController.clear();
-    cycleController.clear();
   }
 
   @override
@@ -46,67 +49,85 @@ class _AddSubscriptionFormState extends State<AddSubscriptionForm> {
     // Clean up the controller when the widget is disposed.
     providerController.dispose();
     priceController.dispose();
-    paymentController.dispose();
-    cycleController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Text(
-        'Add Subscription',
-        style: titleText.copyWith(fontWeight: FontWeight.w500),
-      ),
-      const SizedBox(
-        height: 20,
-      ),
-      TextField(
-        controller: providerController,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Provider',
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(children: [
+        Text(
+          'Add Subscription',
+          style: titleText.copyWith(fontWeight: FontWeight.w500),
         ),
-      ),
-      const SizedBox(
-        height: 10,
-      ),
-      TextField(
-        controller: priceController,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Price',
+        const SizedBox(
+          height: 20,
         ),
-      ),
-      const SizedBox(
-        height: 10,
-      ),
-      const SizedBox(
-        height: 10,
-      ),
-      TextField(
-        controller: paymentController,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Payment Method',
+        TextField(
+          controller: providerController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Provider',
+          ),
         ),
-      ),
-      const SizedBox(
-        height: 10,
-      ),
-      TextField(
-        controller: cycleController,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'Payment Cycle',
+        const SizedBox(
+          height: 10,
         ),
-      ),
-      const SizedBox(
-        height: 10,
-      ),
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
+        TextField(
+          controller: priceController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Price',
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextField(
+          controller: paymentController,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Payment Method',
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        // row with payment cycle and a picker that chooses which payment cycle the subscription has
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Payment Cycle',
+              style: subTitle.copyWith(fontWeight: FontWeight.w500),
+            ),
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: subTitle.copyWith(fontWeight: FontWeight.w500),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? newValue) {
+                setState(() => dropdownValue = newValue!);
+              },
+              items: cycleValues.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value, style: subTitle),
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
@@ -129,10 +150,10 @@ class _AddSubscriptionFormState extends State<AddSubscriptionForm> {
             )
           ],
         ),
-      ),
-      ElevatedButton(
-          onPressed: _createSubscription,
-          child: const Text('Add Subscription')),
-    ]);
+        ElevatedButton(
+            onPressed: _createSubscription,
+            child: const Text('Add Subscription')),
+      ]),
+    );
   }
 }
